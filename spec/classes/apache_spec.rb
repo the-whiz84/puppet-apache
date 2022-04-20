ecae531070f5819b6c013f93ff21db8661aab94a
Radu Chiriac <radu.chiriac@zero-dawn.eu>
2022-04-20 08:11:50 -0400

    Finished configuring the apache module

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -6,8 +6,24 @@ describe 'apache' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+#     let :params do {
+#       install_name: 'apache',
+#       install_ensure: 'present',
+#     }
+#
+      it { is_expected.to contain_class('apache::install') }
+      it { is_expected.to contain_class('apache::config') }
+      it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
+      
+      case os_facts[:osfamily]
+      when 'Debian'
+        it { is_expected.to contain_package('apache2').with_ensure('present') }
+        it { is_expected.to contain_service('apache2').with(ensure: 'running', enable: true, hasrestart: true) }
+      when 'RedHat'    
+        it { is_expected.to contain_package('httpd').with_ensure('present') }
+        it { is_expected.to contain_service('httpd').with(ensure: 'running', enable: true, hasrestart: true) }
+      end
     end
   end
 end
